b6c21693026f6c696c99f2dd4140c2e00536f45d
Piotr Janiszewski <piotr.3.janiszewski@gmail.com>
2025-03-09 16:06:21 +0100

    Added lookup tables, pushed forward work on the cube marcher and fixed issues with the B A L L

--- SDF/SDF.gd
+++ SDF/SDF.gd
@@ -1,9 +1,18 @@
 @icon("res://SDF/sdf_icon.png")
 class_name SDF
+## A class for defining SDF's, for further rendering to a mesh in `CubeMarcher`
+## NOTE: In order for this class to work correctly, you need to provide implementations for the required functions
 extends Node
 
-func get_value_at(input_position :Vector3):
-	return _calculate_value_at(input_position)
+## Returns the distance to the implicit surface
+func get_value_at(input_position :Vector3) -> float:
+	assert(false, "Error: \"get_value_at(input_position :Vector3)\" must be overwritten by an inheriting class. Overwrite this function with your own signed distance function.")
+	return 1.0
 	
-func _calculate_value_at(input_position):
-	assert(false, "Error: \"_calculate_value_at\" must be overwritten by an inheriting class. Overwrite this function with your own signed distance function.")
+func get_negative_bound() -> Vector3:
+	assert(false, "Error: \"get_negative_bound()\" must be overwritten by an inheriting class. Overwrite this function with your own signed distance function.")
+	return Vector3(0.0, 0.0, 0.0)
+	
+func get_positive_bound() -> Vector3:
+	assert(false, "Error: \"get_positive_bound()\" must be overwritten by an inheriting class. Overwrite this function with your own signed distance function.")
+	return Vector3(0.0, 0.0, 0.0)
--- SDF/inheriting sdfs/Ball.gd
+++ SDF/inheriting sdfs/Ball.gd
@@ -4,9 +4,15 @@ class_name Ball
 var radius
 var origin
 
-func _init(c_origin :Vector3 = Vector3(0.0, 0.0, 0.0), c_radius :float = 0.0):
+func _init(c_origin :Vector3 = Vector3(0.0, 0.0, 0.0), c_radius :float = 1.0):
 	radius = c_radius
 	origin = c_origin
 
-func _calculate_value_at(input_position :Vector3):
+func get_value_at(input_position :Vector3):
 	return (input_position - origin).length() - radius
+
+func get_negative_bound() -> Vector3:
+	return origin - radius * Vector3(1.0, 1.0, 1.0)
+	
+func get_positive_bound() -> Vector3:
+	return origin + radius * Vector3(1.0, 1.0, 1.0)
