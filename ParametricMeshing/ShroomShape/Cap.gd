79ba30a7efb07a9de29a2cbc95050be63c549f80
Piotr Janiszewski <piotr.3.janiszewski@gmail.com>
2025-03-15 17:33:56 +0100

    Added saving button for the test scene, and intorduced structure to Mushroom generation

---
+++ ParametricMeshing/ShroomShape/Cap.gd
@@ -0,0 +1,5 @@
+extends ParametricSolid
+class_name Cap
+
+func get_cap_origin():
+	assert(false, "This is a virtual function, it may not be invoked directly")
---
+++ ParametricMeshing/ShroomShape/Leg.gd
@@ -0,0 +1,9 @@
+extends ParametricSolid
+
+class_name Leg
+
+func get_cap_direction():
+	assert(false, "This is a virtual function, it may not be invoked directly")
+	
+func get_cap_origin():
+	assert(false, "This is a virtual function, it may not be invoked directly")
