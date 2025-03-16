e77d94a4986daec8740f1734b1f50040809dde20
Piotr Janiszewski <piotr.3.janiszewski@gmail.com>
2025-03-16 05:45:24 +0100

    Fixed the bug with shrooms not disappearing between recommits of the shape

--- ParametricMeshing/ShroomShape/ShroomShape.gd
+++ ParametricMeshing/ShroomShape/ShroomShape.gd
@@ -4,6 +4,9 @@ class_name ShroomShape
 var _cap
 var _leg
 
+const _cap_mesh_name = "CapMesh"
+const _leg_mesh_name = "LegMesh"
+
 func _init(c_cap :Cap = null, c_leg :Leg = null):
 	set_cap(c_cap)
 	set_leg(c_leg)
@@ -21,15 +24,23 @@ func get_leg():
 	return _leg
 
 func commit():
+	if (get_node(_cap_mesh_name) != null):
+		get_node(_cap_mesh_name).free()
+	if (get_node(_leg_mesh_name) != null):
+		get_node(_leg_mesh_name).free()
+	
 	var cap_mesh = ParametricPolarMesher.get_mesh(_cap)
 	var leg_mesh = ParametricPolarMesher.get_mesh(_leg)
 	var cap_to_leg_translation = _leg.get_cap_origin() - _cap.get_cap_origin()
 	var cap_to_leg_rotation = _leg.get_cap_direction()
 	
 	var cap_mesh_instance = MeshInstance3D.new()
+	cap_mesh_instance.name = _cap_mesh_name
 	cap_mesh_instance.mesh = cap_mesh
 	cap_mesh_instance.transform.origin = _cap.get_cap_origin()
+	
 	var leg_mesh_instance = MeshInstance3D.new()
+	leg_mesh_instance.name = _leg_mesh_name
 	leg_mesh_instance.mesh = leg_mesh
 	
 	cap_mesh_instance.position = cap_to_leg_translation
