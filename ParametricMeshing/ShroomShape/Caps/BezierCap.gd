28fca39deb9d0ef9a48e8e654c51512f9cc4489f
Piotr Janiszewski <piotr.3.janiszewski@gmail.com>
2025-03-16 05:50:27 +0100

    The BezierCap has a more natural shape

--- ParametricMeshing/ShroomShape/Caps/BezierCap.gd
+++ ParametricMeshing/ShroomShape/Caps/BezierCap.gd
@@ -5,7 +5,7 @@ func _init(c_seed = null):
 	if (c_seed != null):
 		seed(c_seed)
 	
-	_bezier_points = [Vector2(0.0, 0.0), Vector2(0.1, 0.0), Vector2(-0.1, -0.1), Vector2(0.7, -0.3), Vector2(0.7, -0.6), Vector2(0.8, -0.7), Vector2(0.4, -1.0)]
+	_bezier_points = [Vector2(0.0, 0.0), Vector2(0.1, 0.0), Vector2(-0.1, -0.1), Vector2(0.7, -0.3), Vector2(0.3, -0.6), Vector2(0.8, -0.7), Vector2(0.4, -1.0)]
 
 func get_value_at(parameter :float, angle :float):
 	if (parameter == 0.0):
@@ -14,4 +14,4 @@ func get_value_at(parameter :float, angle :float):
 	return Vector3(cap_point_rotational.x * cos(angle), cap_point_rotational.y, cap_point_rotational.x * sin(angle))
 
 func get_cap_origin():
-	return Vector3(0.0, _bezier_points[-1].y / 2.0, 0.0)
+	return Vector3(0.0, _bezier_points[-1].y * 0.9, 0.0)
