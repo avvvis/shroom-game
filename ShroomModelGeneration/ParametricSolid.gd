7839ad1faff4477c9bc714bf2b4aa5b01bf454b9
Piotr Janiszewski <piotr.3.janiszewski@gmail.com>
2025-03-20 00:46:25 +0100

    Improved the file structure + small documentation fixes

--- ShroomModelGeneration/ParametricSolid.gd
+++ ShroomModelGeneration/ParametricSolid.gd
@@ -1,8 +1,7 @@
 class_name ParametricSolid
 
 ## * parameter in [0, 1]
-## 		NOTE: Please specify the function for parameter = 0. That point is going to be the origin of the mesh
-## * angle in [0, 2π]
+## * angle in [0, 2π)
 func get_value_at(parameter :float, angle :float) -> Vector3:
 	assert(false, "This is a virtual function, it may not be invoked directly")
 	return Vector3(0.0, 0.0, 0.0)
