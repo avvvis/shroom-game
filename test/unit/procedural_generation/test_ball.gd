383f8acbb160f77ddc20fe61b4bdabd7e4dc545f
Piotr Janiszewski <piotr.3.janiszewski@gmail.com>
2025-04-06 06:56:26 +0200

    Removed redundant files and moved UI tests to separate directory

--- test/unit/procedural_generation/test_ball.gd
+++
@@ -1,9 +0,0 @@
-extends MeshGenericTests
-
-func before_all():
-	model_script_path = "res://test/unit/procedural_generation/Ball.gd"
-	generate_model()
-
-func test_verticies_lie_on_ball():
-	for i in range(0, model.get_vertex_count()):
-		assert_almost_eq(model.get_vertex(i).length(), 1.0, 0.000001)
