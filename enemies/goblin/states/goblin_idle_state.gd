61b34ee7dda42327937d281dae7ade5a46fed83e
Pabianp <119768894+Pabianp@users.noreply.github.com>
2025-04-25 15:48:53 +0200

    repair goblin bugs

--- enemies/goblin/states/goblin_idle_state.gd
+++ enemies/goblin/states/goblin_idle_state.gd
@@ -3,13 +3,14 @@ extends IdleState
 @export var state_duration_min : float = 0.5
 @export var state_duration_max : float = 1.5
 
-var wander_state = move_state
+var wander_state: MoveState
 
 var timer: float = 0
 
 func enter() -> void:
 	super()
 	timer = randf_range(state_duration_min, state_duration_max)
+	wander_state = move_state
 
 func process(delta: float) -> State:
 	timer -= delta
