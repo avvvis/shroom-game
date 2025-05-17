f10d5a4c891763221d2b4c414cc5f5845b992db9
Piotr Janiszewski <piotr.3.janiszewski@gmail.com>
2025-05-17 11:20:56 +0200

    Implemented SpeciesRegistry.generate_shroom

--- Ingredients/SpeciesRegistry.gd
+++ Ingredients/SpeciesRegistry.gd
@@ -1,14 +1,22 @@
 extends Node
 
-var generated_species: Array = []
+var generated_species: Array[IngredientSpecies] = []
 
+## Adds a new species to the registery
 func add_species(species: IngredientSpecies = null):
 	if species == null:
 		species = GlobalFamiliesRandomizer.generate_random_species(GameState._game_seed + generated_species.size())
 	
 	generated_species.append(species)
 
-func get_random_species(seed: int) -> IngredientSpecies:
+## Deletes all the species from the registery
+func clear_species():
+	for species in generated_species:
+		species.queue_free()
+	
+	generated_species = []
+
+func _get_random_species(seed: int) -> IngredientSpecies:
 	if generated_species.size() == 0:
 		push_error("No species in registry")
 	
@@ -17,7 +25,13 @@ func get_random_species(seed: int) -> IngredientSpecies:
 	
 	return generated_species[random.randi_range(0, generated_species.size() - 1)]
 
-
-func generate_shroom(seed: int) -> IngredientSpecimen:
- # TODO
- return ExampleShroom.new()
+## Returns a random shroom
+## If seed == 0 then it's all random, if seed != 0 then that seed will be used for the generation
+func generate_shroom(seed: int = 0) -> IngredientSpecimen:
+	var random = RandomNumberGenerator.new()
+	
+	if seed != 0:
+		random.seed = seed
+	
+	var species_id = random.randi_range(0, generated_species.size() - 1)
+	return generated_species[species_id].generate_specimen(random.randi())
