1b76e5131aaa7907442a2eb5dcc20543bace5850
Pabianp <119768894+Pabianp@users.noreply.github.com>
2025-05-24 22:58:48 +0200

    goblins don't like trees anymore;(

--- general/obstacle.gd
+++
@@ -1,2 +0,0 @@
-class_name Obstacle
-extends Area2D
--- general/pathfinding/pathfinding.gd
+++ general/pathfinding/pathfinding.gd
@@ -2,9 +2,13 @@ class_name Pathfinding
 extends Node2D
 
 @onready var ray = $RayCast2D
+@export var vec_length := 20
+var number_of_dir := 8
+var old_interest_importance := 0.1
 
 var obstacles: Array[float] = [0, 0, 0, 0, 0, 0, 0, 0]
 var interest: Array[float] = [0, 0, 0, 0, 0, 0, 0, 0]
+var old_interest: Array[float] = [0, 0, 0, 0, 0, 0, 0, 0]
 var directions: Array[Vector2] = [
 	Vector2(0, -1),
 	Vector2(1, -1),
@@ -16,35 +20,39 @@ var directions: Array[Vector2] = [
 	Vector2(-1, -1),
 ]
 
+
 func _ready() -> void:
-	for i in directions.size():
+	for i in range(number_of_dir):
 		directions[i].normalized()
 	ray.enabled = false
 		
 func prev(i: int) -> int:
-	return (i - 1) % 8
+	return (i - 1) % number_of_dir
 
 func next(i: int) -> int:
-	return (i + 1) % 8
+	return (i + 1) % number_of_dir
 
 func get_movment_direction(target: Vector2) -> Vector2:
+	
 	ray.enabled = true
-	for i in range(8):
-		ray.target_position = directions[i] * 50
+	for i in range(number_of_dir):
+		ray.target_position = directions[i] * vec_length
 		ray.force_raycast_update()  
 		obstacles[i] = 0
+		old_interest[i] = interest[i]
 		interest[i] = directions[i].dot(target)
 		if ray.is_colliding():
 			var collider = ray.get_collider()
-			if collider is Enemy or collider is Obstacle:
+			if collider is not Player:
 				obstacles[i] += 5
-				obstacles[prev(i)] += 3
-				obstacles[next(i)] += 3
+				obstacles[prev(i)] += 2
+				obstacles[next(i)] += 2
 	ray.enabled = false
 	if obstacles.max() == 0:
 		return target.normalized()
-	for i in range(8):
+	for i in range(number_of_dir):
 		interest[i] -= obstacles[i]
+		interest[i] +=  old_interest_importance * old_interest[i]
 	return directions[interest.find(interest.max())].normalized()
 	
 	
